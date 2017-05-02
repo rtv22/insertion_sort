77b1e344850cb6852950b4bd108f854c87b2d599
rtv22 <rtvcojigor@gmail.com>
2017-05-02 13:20:43 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,7 +1,7 @@
 #include <BinaryTree.hpp>
 #include <catch.hpp>
 
-SCENARIO(sort,[sort])
+SCENARIO("sort","[sort]")
 {
   int matrix[2][2] = { { 222, 555 },{ 111, 444 }};
 	int sorted_matrix[2][2] = { { 111, 222 },{ 444, 555 } };
@@ -13,5 +13,5 @@ SCENARIO(sort,[sort])
 	for (int j = 0; j < 2; j++)
 	if (matrix[i][j] == sorted_matrix[i][j])
 	check = true;
-	REQUARE(check == true)
+	REQUARE(check == true);
 }
